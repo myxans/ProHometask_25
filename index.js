64a89304a50e5196c96c993c20ccbe0936501c67
myxans <mushenko1992@gmail.com>
2023-03-06 18:54:19 +0200

    add_remove1

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
 let table = document.getElementById("table");
 
+let count = 1;
+
 for (let i = 1; i <= 10; i++) {
     let row = table.insertRow();
     for (j = 1; j <= 10; j++) {
         let cell = row.insertCell();
-        cell.innerHTML = Math.floor((Math.random () * 100))
+        cell.innerText = count++
     }
 }
 
 
-/*let count = 1;
-
-for (let i = 1; i <= 10; i++) {
-    let row = table.insertRow();
-    for (j = 1; j <= 10; j++) {
-        let cell = row.insertCell();
-        cell.innerHTML = count++
-    }
-}*/
